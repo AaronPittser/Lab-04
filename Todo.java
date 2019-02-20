c52553674d31d80d3db8d16d26a85c93d1e4a758
AaronPittser <47228138+AaronPittser@users.noreply.github.com>
2019-02-19 22:35:04 -0600

    Add files via upload

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-19
+OU ID_______________: 113433474
+OU EMAIL____________: aaron.pittser@ou.edu
+
+Tasks:
+1. Complete calculateTwoTokens method in Calculator class
+2. Complete calculateThreeTokens method in Calculator class
+3. Complete execute method in Calculator class
+4. Complete parseAndExecute method in Calculator class
+5. Complete tests in CalculatorTest class
+
+Estimated Time (minutes):
+1. 3
+2. 3
+3. 4
+4. 2
+5. 20
+
+Actual Time (minutes):
+1. 5
+2. 6
+3. 7
+4. 15
+5. 18
+*/}
